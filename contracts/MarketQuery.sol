e23343aadf42af71659d7c88bd9dbb6972ca0a82
HanShoujun <tank2308635@gmail.com>
2023-12-18 13:54:56 +0800

    update

--- contracts/MarketQuery.sol
+++ contracts/MarketQuery.sol
@@ -28,27 +28,33 @@ struct MarketPair {
 }
 
 contract BotsMarketQuery {
+
+    function getDemoFunction(uint8 first, uint8 second) external pure returns (uint8) {
+        return first+second;
+    }
     
 
-    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (MarketPair[] memory)  {
+    function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
+        uint256[3][] memory result = new uint256[3][](_pairs.length);
+        for (uint i = 0; i < _pairs.length; i++) {
+            (result[i][0], result[i][1], result[i][2]) = _pairs[i].getReserves();
+        }
+        return result;
+    }
+
+    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
         uint256 _allPairsLength = _uniswapFactory.allPairsLength();
         if (_stop > _allPairsLength) {
             _stop = _allPairsLength;
         }
         require(_stop >= _start, "start cannot be higher than stop");
         uint256 _qty = _stop - _start;
-        MarketPair[] memory result = new MarketPair[](_qty);
+        address[3][] memory result = new address[3][](_qty);
         for (uint i = 0; i < _qty; i++) {
             IUniswapV2Pair _uniswapPair = IUniswapV2Pair(_uniswapFactory.allPairs(_start + i));
-            MarketPair memory pair;
-            pair.token0 = _uniswapPair.token0();
-            pair.token1 = _uniswapPair.token1();
-            pair.pairAddress = address(_uniswapPair);
-            IERC20 token0 = IERC20(_uniswapPair.token0());
-            IERC20 token1 = IERC20(_uniswapPair.token1());
-            pair.symbol0 = token0.symbol();
-            pair.symbol1 = token1.symbol();
-            result[i] = pair;
+            result[i][0] = _uniswapPair.token0();
+            result[i][1] = _uniswapPair.token1();
+            result[i][2] = address(_uniswapPair);
         }
         return result;
     }
